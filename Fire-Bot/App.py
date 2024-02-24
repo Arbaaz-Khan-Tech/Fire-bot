90ad4d049dae74b3dace3d55b1476229c589a23c
Vipulpadwal15 <vipulpadwal2@gmail.com>
2024-02-24 12:59:42 +0530

    minor changes in app.py

--- Fire-Bot/App.py
+++ Fire-Bot/App.py
@@ -3,54 +3,54 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def helloworld():
     return render_template('index.html')
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     
     return render_template('about.html')   #The first name is for html and second one is for this side
 
-@app.route("/blog")
+@app.route("/blog.html")
 def blog():
     
     return render_template('blog.html')  
 
 
-@app.route("/contact")
+@app.route("/contact.html")
 def contact():
     
     return render_template('contact.html')  
 
-@app.route("/detail")
+@app.route("/detail.html")
 def detail():
     
     return render_template('detail.html')  
 
 
 
-@app.route("/price")
+@app.route("/price.html")
 def price():
     
     return render_template('price.html')  
 
 
 
-@app.route("/service")
+@app.route("/service.html")
 def service():
     
     return render_template('service.html')  
 
 
 
-@app.route("/team")
+@app.route("/team.html")
 def team():
     
     return render_template('team.html')  
 
 
 
-@app.route("/testimonial")
+@app.route("/testimonial.html")
 def testimonial():
     
     return render_template('testimonial.html')  
